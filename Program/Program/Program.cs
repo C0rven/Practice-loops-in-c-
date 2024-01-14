318f500a3ca7d9e85b355410e0d830f287cda7a7
C0rven <66511620+C0rven@users.noreply.github.com>
2024-01-14 13:51:09 +0300

    Add files via upload

---
+++ Program/Program/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//2:08:56 / 9:19:55
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int playerHealth = 100;
+            int playerDamage = 10;
+            int enemyHealth = 100;
+            int enemyDamage = 15;
+
+
+            while (playerHealth > 0 && enemyHealth >0 )
+            {
+                playerHealth -= enemyDamage;
+                enemyHealth -= playerDamage;
+                Console.WriteLine(playerHealth + "player.");
+                Console.WriteLine(enemyHealth + "enemy.");
+            }
+
+             if (playerHealth <= 0 & enemyHealth <= 0)
+            {
+                Console.WriteLine("Draw");
+            } 
+                else if (playerHealth <= 0)
+            {
+                Console.WriteLine("Winner Player");
+            }
+
+             else if (enemyHealth <= 0)
+            {
+                Console.WriteLine("Winner Enemy");
+            }
+
+        }
+    }
+}
